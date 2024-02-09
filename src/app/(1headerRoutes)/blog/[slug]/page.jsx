f7031f145d0d11aed60cba2777b1326b35d45864
Coloby <ferinx@proton.me>
2024-02-09 22:37:07 +0100

    feat: adds sticky Table of content in "sl" media query

--- src/app/(1headerRoutes)/blog/[slug]/page.jsx
+++ src/app/(1headerRoutes)/blog/[slug]/page.jsx
@@ -15,17 +15,22 @@ const Page = async ({ params }) => {
   const Component = await getMdxComp("blog", params.slug)
 
   return (
-    <section className=' h-fit max-w-full md:max-w-prose prose prose-purple sm:prose-lg sm:prose-code:text-base prose-p:dark:text-[#D9D9D9]  prose-li:dark:text-[#D9D9D9] prose-img:rounded-xs  dark:prose-invert'>
-      <article className="">
-        <div className="flexy flex-col align-top">
-          <div className=" flex items-center justify-center h-[176px] max-w-[340px] w-full overflow-hidden rounded-xs m-0 mb-6 ">
-            <img className="object-contain xs:object-cover w-full h-full rounded-xs !m-0" src={frontmatter.thumbnail ? "/assets/routes_specific/blog/"+frontmatter.thumbnail : `https://picsum.photos/500/500?random=${frontmatter.index}`} alt="" />
+    <section className='flexy !items-start gap-20 h-fit !max-w-full w-full md:max-w-prose prose prose-purple sm:prose-lg sm:prose-code:text-base prose-p:dark:text-[#D9D9D9]  prose-li:dark:text-[#D9D9D9] prose-img:rounded-xs  dark:prose-invert'>
+      <div className="absolute right-[45px] top-[120px] max-w-[390px] w-full h-full hidden sl:block pb-[320px]">
+        <div className="sticky top-[80px] h-fit w-full "><TOCComponent platform={"desktop"} /></div>
+      </div>
+      <article className="w-full max-w-prose">
+        <div className="w-full">
+          <div className="flexy flex-col align-top">
+            <div className=" flex items-center justify-center h-[176px] max-w-[340px] w-full overflow-hidden rounded-xs m-0 mb-6 ">
+              <img className="object-contain xs:object-cover w-full h-full rounded-xs !m-0" src={frontmatter.thumbnail ? "/assets/routes_specific/blog/"+frontmatter.thumbnail : `https://picsum.photos/500/500?random=${frontmatter.index}`} alt="" />
+            </div>
           </div>
+          <span className="flex flex-wrap gap-x-8 gap-y-1 mb-4"><address>{frontmatter.authors}</address><time>{frontmatter.publishDate}</time><span>{frontmatter.readingTime}</span></span>
+          <h1 className="">{frontmatter.title}</h1>
+          <div className="lead text-primary_text_color">{frontmatter.description}</div>
+          <div className="sl:hidden"><TOCComponent platform={"mobile"} open={false} /></div>
         </div>
-        <span className="flex flex-wrap gap-x-8 gap-y-1 mb-4"><address>{frontmatter.authors}</address><time>{frontmatter.publishDate}</time><span>{frontmatter.readingTime}</span></span>
-        <h1 className="">{frontmatter.title}</h1>
-        <div className="lead text-primary_text_color">{frontmatter.description}</div>
-        <TOCComponent />
         <div className="">
           <Component />
         </div>
--- src/lib/mdx/mdxManager.js
+++ src/lib/mdx/mdxManager.js
@@ -51,28 +51,44 @@ export const getTOCComponentFromSlug = async (fileNameWExt) => {
   const { processedMDX } = await useUnifiedPipeline(rawMDX)
   const TOC = processedMDX.data.toc
 
-  const TOCComponent = () => {
+  const TOCComponent = ({ platform = "mobile", open = true }) => {
     return (
       <nav className="mb-12">
-        <Accordion type="single" collapsible>
-          <AccordionItem value="item-1">
-            <AccordionTrigger>Table of contents</AccordionTrigger>
+        <Accordion type="single" collapsible defaultValue={"item-"+open}>
+          <AccordionItem value={`item-${open && true}`}>
+            <AccordionTrigger><div className="text-xl">Table of contents</div></AccordionTrigger>
             <AccordionContent>
               <ul className="space-y-2 not-prose !list-disc">
-                {TOC.map((header) => {
-                  const paddingVariants = {
-                    1: 'text-xl',
-                    2: 'text-xl !list-none',
-                    3: 'ml-[25px] sm:ml-[45px] text-lg !list-none',
-                    4: 'ml-[50px] sm:ml-[70px] text-base',
+                {TOC.map((header, i) => {
+                    const headerVariants = {
+                    mobile : {
+                      1: '',
+                      2: 'text-xl !list-none',
+                      3: 'ml-[25px] sm:ml-[45px] text-lg !list-none',
+                      4: 'ml-[50px] sm:ml-[70px] text-base !list-none',
+                    },
+                    desktop : {
+                      1: '',
+                      2: 'text-lg !list-none',
+                      3: 'ml-[35px] text-base !list-none',
+                      4: 'ml-[70px] text-base !list-none',
+                    }
                   }
-                  let paddingLevel = header.level
+                  let headerLevel = header.level
                   return (
-                    <li key={header.id} className={paddingVariants[paddingLevel] + " !mb-4"}>
-                      <a href={`#${header.id}`} className=" text-[#9333ea] dark:text-[hsl(271,91%,70%)] hover:underline">
-                        {header.text.slice(1)}
-                      </a>
-                    </li>
+                    // <li key={header.id} className={headerVariants[platform][headerLevel] + `!list-none mb-4 ${headerLevel === 3 || 4 ? "!mt-0" : ""} ${TOC[headerLevel + 1].level === 3 || 4 ? "!mb-0" : ""}`}>
+                    <>
+                      {TOC[headerLevel].level === 2 && i > 0 ?
+                      <div className="!h-[0.5px] !border-[0.5px] w-[90%] border-[#374151] rounded-xs !my-5"></div>
+                      :
+                      ""
+                      }
+                      <li key={header.id} className={headerVariants[platform][headerLevel] + `not-prose mt-0 !list-none ${headerLevel === 2 ? "!mb-5 !mt-5" : headerLevel === 3 ? "!mb-4 !mt-4" : "!mb-0 !mt-0"}`}>
+                        <a href={`#${header.id}`} className=" text-primary_text_color/80 hover:underline hover:text-primary_text_color">
+                          {header.text.slice(1)}
+                        </a>
+                      </li>
+                    </>
                   )
                 })}
               </ul>
