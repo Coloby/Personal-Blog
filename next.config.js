33b198bc2073bca4be4f3cf9c8db0027213a100c
Coloby <ferinx@proton.me>
2024-12-22 18:39:08 +0100

    pnpx create-payload-app - but /admin doesn't work
    2 Warings:
    pg package resolving 2 different versions
    index.d.ts not found from the node_modules

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const { withPayload } = require("@payloadcms/next/withPayload");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: 'dist',
@@ -50,4 +51,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+module.exports = withPayload(nextConfig)
