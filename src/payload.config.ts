19ef397a034d8ae3407e9c304cbc25af744805d5
Coloby <ferinx@proton.me>
2024-12-23 10:57:48 +0100

    feat: s3 supabase storage minimal install & config

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -6,6 +6,7 @@ import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
+import { s3Storage } from "@payloadcms/storage-s3"
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
@@ -34,6 +35,23 @@ export default buildConfig({
   sharp,
   plugins: [
     payloadCloudPlugin(),
+    s3Storage({
+      collections: {
+        media: {
+          prefix: 'media',
+        }
+      },
+      bucket: process.env.S3_BUCKET as string,
+      config: {
+        forcePathStyle: true, // fixes problems if you use supabase
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY_ID as string,
+          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY as string,
+        },
+        region: process.env.S3_REGION,
+        endpoint: process.env.S3_ENDPOINT,
+      },
+    }),
     // storage-adapter-placeholder
   ],
 })
